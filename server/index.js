8a745afd0e074c9ffb35fc4fb3155cef878f67ac
Pankaj1041 <pankaj1041.cse19@chitkara.edu.in>
2022-06-26 23:16:43 +0530

    make better

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
+const path =require('path');
+const res = require("express/lib/response");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-
+const PORT =process.env.PORT||3000;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -23,8 +25,23 @@ mongoose.connect(process.env.MONGO_URL, {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+//------------------
 
-const server = app.listen(process.env.PORT ||3000, () =>
+const dirname=path.resolve();
+if(process.env.NODE_ENV ==="production"){
+app.use(express.static(path.join(dirname,"/public/build")));
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(dirname,"public","build","index.html"));
+});
+}
+else{
+  app.get("/", (req,res)=>{
+    res.send("API is Running Successfully");
+  });
+}
+
+//---------------
+const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
